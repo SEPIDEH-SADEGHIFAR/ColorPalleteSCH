e8e89f280d8f490eb8e08bdd62c92260357bd73b
sepideh <sepideh.sadeghifar12@gmail.com>
2026-02-10 10:49:22 +0100

    Initial Commit

---
+++ ColorPalleteSCH/ColorPalleteSCHApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPalleteSCHApp.swift
+//  ColorPalleteSCH
+//
+//  Created by seyedeh sepideh sadeghi far on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ColorPalleteSCHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
